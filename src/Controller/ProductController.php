9de9329b495864e57820024d460a83e60099c9b9
imzhai <jue.yguel@gmail.com>
2019-08-23 16:01:53 +0200

    form demo

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,7 @@ use App\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends AbstractController
 {
@@ -35,4 +36,41 @@ class ProductController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+    /** 
+     * @Route("/product/demo", name="product_demo")
+    */
+    public function demo()
+    {
+        // Récupérer le repository de l'entité Product
+        $productRepository = $this->getDoctrine()->getRepository(Product::class);
+        // Récupérer tous les produits 
+        $products = $productRepository->findAll();
+        // dump($products);
+
+        // Récupérer le produit avec l'id 3
+        $product2 = $productRepository->find(3);
+
+        // Récupérer le produit qui se nomme iPhone X
+
+        // $productRepository->findOneByName('iPhone X');
+        $product3 = $productRepository->findOneBy(['name' => 'iPhone X']);
+
+        // Récupérer tous les produits qui coutent 1500 euros exacement
+        $products1500 = $productRepository->findByPrice(1500);
+
+
+        return $this->render('product/demo.html.twig', [
+            'products' => $products,
+            'product2' => $product2,
+            'product3' => $product3,
+            'products1500' => $products1500
+
+
+
+        ]);
+
+
+    }
+
 }
