3ab06d3eaf421f425bbc309fe0d347b89f787731
imzhai <jue.yguel@gmail.com>
2019-08-26 15:12:28 +0200

    user tab

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -10,13 +11,26 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <30; $i++){
-        $product = new Product();
-        $product->setName('iPhone Pépère'. " ".$i);
-        $product->setDescription('Un smartPhone'. " ".$i);
-        $product->setPrice(rand(500, 1500));
-        $manager->persist($product);
-    }
+
+        // Créer utilisateur
+        $users = []; // Le tableau vas nous aider à stocker les instances des user
+        for($i =0; $i<10; $i++){
+            $user = new User();
+            $user->setUsername('Username '.$i);
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        // Créer produit
+        for($i = 0; $i <30; $i++){
+            $product = new Product();
+            $product->setName('iPhone Pépère'. " ".$i);
+            $product->setDescription('Un smartPhone'. " ".$i);
+            $product->setPrice(rand(500, 1500));
+            // On associe le produit à une instance de user ($user correspond au dernier user créé)
+            $product->setUser($users[rand(0, 9)]);
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
