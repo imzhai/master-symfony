8deb34bbd1f99e6036d603cd5a380451548bc0fe
imzhai <jue.yguel@gmail.com>
2019-08-26 14:51:27 +0200

    ManytoOne

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -52,6 +52,11 @@ class Product
 
     private $slug;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="products")
+     */
+    private $user;
+
 
     public function getId(): ?int
     {
@@ -111,6 +116,18 @@ class Product
         }
         
         $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
