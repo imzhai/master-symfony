d58c9ad7bb0853c5fe0a53d8e3c805782ea00069
imzhai <jue.yguel@gmail.com>
2019-08-26 10:23:56 +0200

    CRUD

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -39,7 +39,7 @@ class ProductController extends AbstractController
 
     /** 
      * @Route("/product/demo", name="product_demo")
-    */
+     */
     public function demo()
     {
         // Récupérer le repository de l'entité Product
@@ -65,12 +65,93 @@ class ProductController extends AbstractController
             'product2' => $product2,
             'product3' => $product3,
             'products1500' => $products1500
+        ]);
+    }
+
+    /**
+     * @Route ("/product", name="products")
+     */
+    public function list()
+    {
+        $productRepository = $this->getDoctrine()->getRepository(Product::class);
+        $products = $productRepository->findAll();
+
+        return $this->render('product/product.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    /** 
+     * @Route ("/product/{id}", name="product_show")
+     */
+    public function show(Product $product) // Dans la version commentée, on mettra le paramètre $id
+    {
+        // $product = $this->getDoctrine()
+        //     ->getRepository(Product::class)
+        //     ->find($id);
+
+        // // Si le produit n'existe pas
+        // if(!$product){
+        //     throw $this->createNotFoundException();
+        // }
 
 
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+
+    /** 
+     * @Route("/product/edit/{id}", name="product_edit") 
+     */
+    public function edit(Request $request,Product $product)
+    {
+        //Sert juste à créer le template dans la vue
+        $form = $this->createForm(ProductType::class, $product); 
+
+        $form->handleRequest($request);
+
+        // Quand le formulaire est valide
+        if ($form->isSubmitted() && $form->isValid())
+        {
+        // On récupère Doctrine pour gérer la BDD
+        $entityManager = $this->getDoctrine()->getManager();
+        // Execute la requête (INSERT...)
+        $entityManager->flush();
+
+        $this->addFlash('success', 'le produit a été modifié');
+
+        return $this->redirectToRoute('products');
+
+        }
+        
+
+
+        return $this->render('product/edit.html.twig', [
+            'form' => $form->createView(),
 
         ]);
 
-
     }
 
+    /** 
+     * @Route ("/product/delete/{id}", name="product_delete") 
+     */
+    public function delete(Product $product)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($product);
+        // On execute la requête DELETE
+        $entityManager->flush();
+
+        $this->addFlash('success', 'le produit a été supprimé');
+        //redirection vers la liste
+        return $this->redirectToRoute('product_list');
+    }
+
+
+
+
 }
+
