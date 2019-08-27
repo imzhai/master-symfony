5c4a70aa1168f5095962a32c33bf1940b0c298e8
imzhai <jue.yguel@gmail.com>
2019-08-27 13:04:09 +0200

    generate password

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,15 +5,30 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use App\Entity\Product;
 use App\Entity\Category;
+use App\Entity\Customer;
 use App\Entity\Tag;
 
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder )
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        // Créer utilisateur
+        $customer = new Customer();
+        $customer->setEmail('bobicolor@gmail.com');
+        // On génère le hash du mot de passe test
+        $encodedPassword = $this->passwordEncoder->encodePassword($customer, 'test');
+        $customer->setPassword($encodedPassword);
+        $manager->persist($customer);
 
         // Créer utilisateur
         $users = []; // Le tableau vas nous aider à stocker les instances des user
